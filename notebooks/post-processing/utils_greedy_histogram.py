bc82a43b8af5bbf442a5166b7a8ff7cc67094ec3
PastelBelem8 <cbelem@uci.edu>
2024-09-30 15:55:02 -0700

    Add notebook w/ models greedy histogram: top-k open ai methodology

--- notebooks/post-processing/utils_greedy_histogram.py
+++ notebooks/post-processing/utils_greedy_histogram.py
@@ -54,3 +54,35 @@ def create_histogram_humans(
     nhistograms = {u: {bc: cs / total[u] for bc, cs in uhistograms[u].items()} for u in uncertainty_expressions}
 
     return uhistograms, nhistograms
+
+
+
+def create_histogram_for_top_k_logprobs__openai(
+    df: pd.DataFrame,
+    id_col: str,
+    number_col: str,
+    unc_col: str,
+    uncertainty_expressions: List[str],
+    bin_center: float,
+    bin_offset: float,
+) -> Tuple[Dict[str, float]]:
+    # Select the subset of rows concerning the specified uncertainty expressions
+    df = df[df[unc_col].isin(uncertainty_expressions)].copy()
+
+    # Initialize the histogram
+    uhistograms = init_histograms(uncertainty_expressions, bin_center)
+
+    # Determine the bins
+    bins = df[number_col].apply(get_bin_for_val, bin_center=bin_center, bin_offset=bin_offset)
+    # Accumulate per bin
+    for u, b in zip(df[unc_col], bins):
+        uhistograms[u][b] += 1
+
+    # Compute the number of unique statements (identified through id_col)
+    total = {u: df[df[unc_col] == u][id_col].nunique() for u in uncertainty_expressions}
+
+    # Normalized histograms
+    nhistograms = {u: {bc: cs / total[u] for bc, cs in uhistograms[u].items()} for u in uncertainty_expressions}
+    return uhistograms, nhistograms
+
+    
