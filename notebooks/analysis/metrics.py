23a6dc93e657a4ad97cb94bd7ae6cf51f15bfdb9
PastelBelem8 <cbelem@uci.edu>
2024-10-01 14:25:43 -0700

    Add wasserstein distance computation: notebooks/analysis/Metrics.ipynb

--- notebooks/analysis/metrics.py
+++ notebooks/analysis/metrics.py
@@ -1,11 +1,10 @@
+import pandas as pd
 import numpy as np
 
-# -------------------------
-# add cpa.py dependency
-# -------------------------
-import sys, pathlib, os
-current_filepath = pathlib.Path(os.path.abspath(__file__))
-sys.path.append(current_filepath.parent.parent.parent.parent / "src" / "cpa.py")
+from collections import defaultdict
+from scipy.stats import wasserstein_distance
+from typing import Dict, List
+from cpa import *
 
 
 def absolute_error(dist1: dict, dist2: dict):
@@ -48,3 +47,43 @@ def get_statistics_per_distribution(histogram: dict):
         "min": mins,
         "num_unique": uniques,
     }
+
+
+def assert_valid_prob(hist, axis=1, tol=1e-10):
+    assert ((1-hist.sum(axis=axis)) <= tol).all()
+    assert (np.abs(hist) >= -tol).all().all()
+    assert (np.abs(hist) <= 1+tol).all().all()
+
+
+def assert_same_exprs(test_hist: pd.DataFrame, ref_hist: pd.DataFrame):
+    for t_expr, ref_expr in zip(test_hist.index, ref_hist.index):
+        assert t_expr == ref_expr, f"Expression order mismatch: {t_expr} != {ref_expr}"
+
+
+def compute_wasserstein_distance(
+    model_df: pd.DataFrame,
+    ref_df: pd.DataFrame,
+    uncertainty_expressions: List[str],
+) -> pd.DataFrame:
+    assert_valid_prob(model_df)
+    assert_same_exprs(model_df, ref_df)
+
+    results = defaultdict(list)
+    for expr in uncertainty_expressions:
+        ref_expr_hist = [(float(k), p) for k, p in ref_df.loc[expr].to_dict().items()]
+        model_expr_hist = [(float(k), p) for k, p in model_df.loc[expr].to_dict().items()]
+    
+        ref_expr_hist = sorted(ref_expr_hist, key=lambda x: x[0])
+        model_expr_hist = sorted(model_expr_hist, key=lambda x: x[0])
+        assert all([p[0] == q[0] for p, q in zip(ref_expr_hist, model_expr_hist)]), f"Number mismatch: {p} vs {q}"
+    
+        ref_values, ref_weights = zip(*ref_expr_hist)
+        model_values, model_weights = zip(*model_expr_hist)
+        
+        results["uncertainty_expression"].append(expr)
+        results["distance"].append(wasserstein_distance(
+            u_values=ref_values, u_weights=ref_weights,
+            v_values=model_values, v_weights=model_weights,
+        ))
+    
+    return pd.DataFrame(results)
