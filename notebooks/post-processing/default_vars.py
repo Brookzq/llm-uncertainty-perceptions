3f57504641f30f46972a1fc3f46cffe8e716c785
PastelBelem8 <cbelem@uci.edu>
2024-10-01 12:07:36 -0700

    Add data and notebooks to support appendix analysis llm-belief-correctness

--- notebooks/post-processing/default_vars.py
+++ notebooks/post-processing/default_vars.py
@@ -22,3 +22,43 @@ UNCERTAINTY_EXPRESSIONS = [
   "very unlikely",
   "highly unlikely"
 ]
+
+
+MAPPING_2_CANONIC = {
+    "top-k": {
+        "model": 'model',
+        'uncertainty_expression': "uncertainty_expression",
+        "number_1": "numerical_response", 
+        'name': "speaker_name", 
+        'gender': "speaker_gender", 
+        'template': "template", 
+        'statement_id': "statement_id", 
+        "statement_type": "statement_type", 
+        "statement_uuid": "statement_uuid",
+        'statement': "statement",
+    },
+    "full-prob": {
+        "completion__model": 'model',
+        'uncertainty_expression': "uncertainty_expression",
+        "completion__suffix": "numerical_response",
+        'name': "speaker_name", 
+        'gender': "speaker_gender", 
+        'template': "template", 
+        'statement_id': "statement_id", 
+        "statement_type": "statement_type", 
+        "statement_uuid": "statement_uuid",
+        'statement': "statement",
+    },
+    "sampling-based": {
+        "model": 'model',
+        'uncertainty_expression': "uncertainty_expression",
+        "response_first_number": "numerical_response", 
+        'name': "speaker_name", 
+        'gender': "speaker_gender", 
+        'template': "template", 
+        'statement_id': "statement_id", 
+        "statement_type": "statement_type", 
+        "statement_uuid": "statement_uuid",
+        'statement': "statement",
+    }
+}
